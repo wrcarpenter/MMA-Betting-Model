ef5a39520323d59aa6c1a2e0c93ce2a239788eed
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2023-04-02 09:49:52 -0400

    Add files via upload

--- Code/UFC-Bout-Cleaning.py
+++ Code/UFC-Bout-Cleaning.py
@@ -1,8 +1,16 @@
+"""
 
-# MMA Bout Cleaning 
-# W. Carpenter 
+MMA Bout Cleaning 
 
-#Importing
+Creator: Will Carpenter 
+
+Description: Used for cleaning UFC data scraped online. This will work off the 
+raw data to create a usable panel for model implementation. 
+
+"""
+#%%
+
+# Importing
 import requests
 import csv 
 import re
@@ -70,13 +78,15 @@ df['prev_num_fights'] = df.groupby(['fighter_profile'])['ones'].cumsum() - 1
 df['wks_since_last_fight'] = df.groupby(['fighter_profile'])['event_date'].diff()  # total time between date values
 df['wks_since_last_fight'] = df['wks_since_last_fight'] / np.timedelta64(1, 'W')   # this includes some NaN
 
-
-# categorical should be good - 0,1,2,3,4,5,6,7,8 or something 
-
-
-# Add weeks since second to last fight 
+# Years since first recorded fight 
+df['min_date']    = df.groupby(['fighter_profile'])['event_date'].min()
+df['roster_time'] = (df['event_date'] - df['min_date']) / np.timedelta64(1, 'Y')
  
  
+#%% 
+ 
+# FIGHT RESULTS 
+
 # Result of last fight 
 # Need to breakdown the results by some kind of number pattern 
 # Main fight results (binary variable)
@@ -84,35 +94,35 @@ df['win']        = df['fighter_result'].apply(lambda x: 1 if x == 'W' else 0)
 df['loss']       = df['fighter_result'].apply(lambda x: 1 if x == 'L' else 0)
 df['no_contest'] = df['fighter_result'].apply(lambda x: 1 if x == 'NC' else 0)
 df['draw']       = df['fighter_result'].apply(lambda x: 1 if x == 'D' else 0)
+
 # Granular result metrics (if betting on KO win or something else)
 # there are some other fields included in these results
 df['ko']  = df['method'].apply(lambda x: 1 if x == 'KO/TKO' or x == "TKO - Doctor's Stoppage" or x == "Could Not Continue" else 0)
 df['sub'] = df['method'].apply(lambda x: 1 if x == 'Submission' else 0)
 df['dec'] = df['method'].apply(lambda x: 1 if x == 'Decision - Unanimous' or x == "Decision - Split" or x == "Decision" else 0)
+
 # Defining win categories for potential prediction 
 df['ko_win']  = np.where(((df['ko'] == 1) & (df['win'] == 1 )), 1, 0)
 df['sub_win'] = np.where(((df['sub'] == 1) & (df['win'] == 1 )), 1, 0) 
 df['dec_win'] = np.where(((df['dec'] == 1) & (df['win'] == 1 )), 1, 0) 
 
-# What's next here 
 # height,weight
 # result of last fight (define categorical variables, ko_loss, ko_win, etc. )
 # result of second to last fight
 
 # Shifting for previous fight results 
-
 # Issue here is that you are shifting up another fighters results to be the first observation!!!
 df.groupby('object')['value'].shift()
 
 # Should mark first fight, no previous data 
 df['prev_fight_result'] = 0 
 
-# Neither a win or loss
+# Neither a 
 df['prev_fight_result'] = np.where(((df.groupby('fighter_profile')['win'].shift(1) == 0)&\
-                                    (df.groupby('fighter_profile')['loss'].shift(1) == 0 )),1,df['prev_fight_result'])
+                                    (df.groupby('fighter_profile')['loss'].shift(1) == 0)),1,df['prev_fight_result'])
 # Loss and KO result
 df['prev_fight_result'] = np.where(((df.groupby('fighter_profile')['loss'].shift(1) == 1)&\
-                                    (df.groupby('fighter_profile')['ko'].shift(1) == 1 )),2,df['prev_fight_result'])
+                                    (df.groupby('fighter_profile')['ko'].shift(1) == 1)),2,df['prev_fight_result'])
     
 # Loss and sub result
 df['prev_fight_result'] = np.where(((df.groupby('fighter_profile')['loss'].shift(1) == 1)&\
@@ -121,11 +131,11 @@ df['prev_fight_result'] = np.where(((df.groupby('fighter_profile')['loss'].shift
 # Loss and dec result
 df['prev_fight_result'] = np.where(((df.groupby('fighter_profile')['loss'].shift(1) == 1)&\
                                     (df.groupby('fighter_profile')['dec'].shift(1) == 1 )),4,df['prev_fight_result'])     
-
          
-df['prev_fight_result'] = np.where(((df['dec_win'].shift(1) == 1)), 5, df['prev_fight_result'])
-df['prev_fight_result'] = np.where(((df['sub_win'].shift(1) == 1)), 6, df['prev_fight_result'])
-df['prev_fight_result'] = np.where(((df['ko_win'].shift(1) == 1)), 7, df['prev_fight_result'])
+df['prev_fight_result'] = np.where((df.groupby('fighter_profile')['dec_win'].shift(1) == 1),5,df['prev_fight_result'])
+df['prev_fight_result'] = np.where((df.groupby('fighter_profile')['sub_win'].shift(1) == 1),6,df['prev_fight_result'])
+df['prev_fight_result'] = np.where((df.groupby('fighter_profile')['ko_win'].shift(1) == 1),7,df['prev_fight_result'])
+                                       
 # Encoding 
 # ko_win   = 7
 # sub_win  = 6 
@@ -137,19 +147,44 @@ df['prev_fight_result'] = np.where(((df['ko_win'].shift(1) == 1)), 7, df['prev_f
 # first   = 0
 
 # "Browsing tools 
-browse_columns = ['name', 'fighter_result', 'event_date','shift_test','method', 'win',  'ko', 'sub', 'dec','ko_win', 'prev_fight_result','sub_win', 'dec_win']
+browse_columns = ['name', 'fighter_result', 'event_date', 'method', 'win', 'loss', 'ko', 'sub', 'dec','ko_win', 'prev_fight_result','sub_win', 'dec_win']
 # Browser for de-bugging 
 df_browse = df[browse_columns]
 df_browse_sample = df.loc[df['name'] == 'Jon Jones', browse_columns]
-
 
 # Very useful commands
 df.columns
 df.columns.to_list()
 df = df.drop(columns=['-'])
 
+#%%
+
+# Save down dataset 
+
+df.to_csv('/content/drive/MyDrive/MMA Model/Data/ufcBouts_v6.csv')
 
 #%%
+
+# Create a unique model dataset! 
+
+df_model = df.sample(frac=1).drop_duplicates(subset=['fight_link'])
+
+df = df.drop_duplicates(subset='fighter_profile')
+
+print(df)
+# Explore the dataset 
+print(df_model.columns)
+# Columns in dataset 
+print(df_model.shape[1]) # raw dataset should have 28 columns? 
+# Unique fighters
+print("Number of Unique Fighters: ", len(pd.unique(df_model['name'])))  # unique fighters
+# Number of fights (nows in dataset)
+print("Total Fights Recorded: ", len(df_model))
+# Number of unique fights in the dataset 
+print("Number of Unique Fights: ", len(pd.unique(df['fight_link'])))
+
+
+df_model.to_csv('/content/drive/MyDrive/MMA Model/Data/ufcBouts_v6.csv')
 
 # need to get age since fight 
 
@@ -157,10 +192,6 @@ df = df.drop(columns=['-'])
 # deal with birthdays / blanks 
 # deal with event dates / blanks
  
-
-
-
-
 # convert event dates
 # find all upcoming events and delete? this should be based on dates
 
