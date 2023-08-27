eb4ce4a6c41b72308e45dad3b109deee89717e6f
Will Carpenter <65867547+wrcarpenter@users.noreply.github.com>
2023-08-27 09:02:24 -0400

    updated scraper

--- Code/Moneyline-Odds-Scraper.py
+++ Code/Moneyline-Odds-Scraper.py
@@ -20,47 +20,51 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
 
-# %% 
-
+#%%
 # Add in "headless" browsing
 from selenium.webdriver.chrome.options import Options
 options = Options()
 options.add_argument("--headless=new")
 driver = webdriver.Chrome(options=options)
 
+#%% 
+# Get link data 
+source = 'https://raw.githubusercontent.com/wrcarpenter/MMA-Betting-Model/main/Data/odds-fighter-links.csv'  
+df      = pd.read_csv(source, header=0) 
+df_orig = df 
+del source
+fighter_list = df['link'].values.tolist()
+
 #%%
-
-driver.get('https://www.bestfightodds.com/fighters/Jose-Aldo-659') # example
-
-# links = driver.find_elements(By.CLASS_NAME, "main-row")  # works
-
-# use Selenium to open web pages
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'lxml')
-links = soup.find_all("a")
-
-event_list = []
-fighter_list = []
-url_front_substring = 'https://www.bestfightodds.com'
-
-for l in links:
-    
-    pagelink = str(l.get('href'))
-    
-    if '/events/' in pagelink:
-        print(url_front_substring + pagelink)
+# Aquire new fight links      
+for link in fighter_list: 
         
-    if '/fighters/' in pagelink:
-        print(url_front_substring + pagelink)
-        # to_append = 
+    print("Parsing:  ", link)
+    print(" ")
     
-    else:
-        # skip all other links pulled
-        continue
+    driver.get(link)
+    page_source = driver.page_source
+    soup        = BeautifulSoup(page_source, 'lxml')
+    links       = soup.find_all("a")
+    url_string  = 'https://www.bestfightodds.com'
+    
+    for i in links:
+        pagelink = str(i.get('href'))
+        to_app   = url_string + pagelink
+    
+        if '/fighters/' in to_app:
+            if to_app not in fighter_list:
+                fighter_list.append(to_app)
+        
+#%%
+# Export to excel
+df_fighters = pd.DataFrame(fighter_list, columns = ['link'])
+df_events   = pd.DataFrame(event_list, columns = ['link'])
 
+df_fighters.to_csv('C:/Users/wcarp/OneDrive/Desktop/MMA Bout  Model/Data/odds-fighter-links.csv')
+df_events.to_csv('C:/Users/wcarp/OneDrive/Desktop/MMA Bout  Model/Data/odds-event-links.csv')
 
 #%%
-
 # References 
 # https://selenium-python.readthedocs.io/locating-elements.html
 
